6a56e77d2878da81e2dc4fb46b7b87da5408cfad
Andrew Yeonsoo Han <drewhan90@gmail.com>
2016-11-07 14:48:45 -0800

    Upload working chart.js
    working chart js.
    Requires connection to data.

--- public_html/compare.php
+++ public_html/compare.php
@@ -218,8 +218,7 @@
 
         <!-- CHART SECTION -->
         <section class="chart-section">
-			<canvas id="chart">
-			</canvas>
+			<canvas id="myChart"></canvas>
         </section>
     </main>
 
--- public_html/js/app.js
+++ public_html/js/app.js
@@ -9,46 +9,39 @@ $(document).ready(function () {
     //////////////////////////
     /// Chart.js by Andrew ///
     //////////////////////////
-    import Chart from '../modules/chart/chart.js/src/chart.js';
-    let myChart = new Chart({...});
-
-    const ctx = $('#chart');
-    const myChart = new Chart(ctx, {
-    	type: 'bar', /// type of chart 
+    console.log(Chart);
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+    	type: 'pie', /// type of chart 
     	data: {
-    		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    		datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-	        }]
+    		labels: ["Liberal", "Democratic", "Conservative", "Green"], /// chart label array
+    		datasets: [
+    			{
+		            label: '# of Votes',
+		            data: [60, 20, 10, 10], /// Data, # of votes
+		            backgroundColor: [
+		                'rgba(255, 99, 132, 0.2)',
+		                'rgba(54, 162, 235, 0.2)',
+		                'rgba(255, 206, 86, 0.2)',	
+		                'rgba(255, 159, 64, 0.2)'
+		            ],
+		            hoverBackgroundColor: [
+		                'rgba(255,99,132,1)',
+		                'rgba(54, 162, 235, 1)',
+		                'rgba(255, 206, 86, 1)',
+		                'rgba(255, 159, 64, 1)'
+		            ]
+	           }
+           ]
 	    },
 	    options: {
-	        scales: {
-	            yAxes: [{
-	                ticks: {
-	                    beginAtZero:true
-	                }
-	            }]
-	        }
-	    }
-	});
-    	}
-    })
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });   
 });
