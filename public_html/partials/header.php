1a5deaf064b84ac3a5ce5de581252bbc64e65c5c
mside <michaelside0@gmail.com>
2016-11-06 21:52:02 -0800

    merging

--- public_html/partials/header.php
+++ public_html/partials/header.php
@@ -7,13 +7,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Make Your Vote Up</title>
     <link rel="stylesheet" href="css/foundation.css">
-    <!-- FlexSilder -->
     <link rel="stylesheet" href="css/flexslider.css" type="text/css">
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     <link rel="stylesheet" href="css/app.css">
 </head>
 
 <body>
-    <header class="page-header">
+    <header class="page-header row">
+    	<ul class="mobile-hidden">
+            <li class="inline left"><img src="img/final-logo.png" alt="Make Your Vote Up Logo" class="logo"></li>
+	    	<li class="header-li inline right">
+                <a class="header-anchor desktop-anchor" href="party.php">
+                    Party Info
+                </a>
+            </li>
+	    	<li class="header-li inline right">
+                <a class="header-anchor desktop-anchor" href="mp.php">
+                    Who's My Mp
+                </a>
+            </li>
+	    	<li class="header-li inline right">
+                <a class="header-anchor desktop-anchor" href="compare.php">
+                    Compare Parties
+                </a>
+            </li>
+	    </ul>
+	    <ul id="mobile-header">
+            <li><img src="img/final-logo.png" alt="Make Your Vote Up Logo" class="logo"></li>
+	    	<li class="header-li small-12 columns">
+                <a class="header-anchor" href="party.php">
+                    Party Info
+                </a>
+            </li>
+	    	<li class="header-li small-12 columns">
+                <a class="header-anchor" href="mp.php">
+                    Who's My Mp
+                </a>
+            </li>
+	    	<li class="header-li small-12 columns">
+                <a class="header-anchor" href="compare.php">
+                    Compare Parties
+                </a>
+            </li>
+	    </ul>
+	   <button onclick="function mobileMenu()" class="header-anchor mobile-only right"> &#9776; </button>
     </header>
+
+    <script>
+        function mobileMenu(){
+            document.getElementById("mobile-header").style.display = "block";
+        }
+    </script>
